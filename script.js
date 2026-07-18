cdb389ec9c53d562646b06cc9ccfa07b5994f767
ayanshpunia581-stack <ayanshpunia581@gmail.com>
2026-07-18 20:46:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
+const canvas=document.getElementById("game");
+const ctx=canvas.getContext("2d");
 
+let player={
+x:450,
+y:300,
+w:30,
+h:30,
+speed:5
+};
+
+let keys={};
+
+document.addEventListener("keydown",e=>{
+keys[e.key.toLowerCase()]=true;
+});
+
+document.addEventListener("keyup",e=>{
+keys[e.key.toLowerCase()]=false;
+});
+
+function update(){
+
+if(keys["w"]||keys["arrowup"]) player.y-=player.speed;
+if(keys["s"]||keys["arrowdown"]) player.y+=player.speed;
+if(keys["a"]||keys["arrowleft"]) player.x-=player.speed;
+if(keys["d"]||keys["arrowright"]) player.x+=player.speed;
+
+}
+
+function draw(){
+
+ctx.fillStyle="#4caf50";
+ctx.fillRect(0,0,900,600);
+
+ctx.fillStyle="gray";
+ctx.fillRect(200,0,100,600);
+ctx.fillRect(500,0,100,600);
+
+ctx.strokeStyle="white";
+ctx.setLineDash([20,20]);
+
+ctx.beginPath();
+ctx.moveTo(250,0);
+ctx.lineTo(250,600);
+ctx.moveTo(550,0);
+ctx.lineTo(550,600);
+ctx.stroke();
+
+ctx.setLineDash([]);
+
+ctx.fillStyle="red";
+ctx.fillRect(player.x,player.y,player.w,player.h);
+
+ctx.fillStyle="white";
+ctx.font="20px Arial";
+ctx.fillText("Mini GTA Demo",20,30);
+ctx.fillText("Move: WASD or Arrow Keys",20,60);
+
+}
+
+function game(){
+
+update();
+draw();
+
+requestAnimationFrame(game);
+
+}
+
+game();
